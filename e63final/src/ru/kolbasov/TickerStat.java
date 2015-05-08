eb0de98e3667a328f8527d42ecb581d27a9587fb
skolbasov@gmail.com <skolbasov@gmail.com>
2015-05-08 19:51:06 +0300

    DescriptiveStatistics added

--- e63final/src/ru/kolbasov/TickerStat.java
+++ e63final/src/ru/kolbasov/TickerStat.java
@@ -4,37 +4,46 @@ import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
 
+import org.apache.commons.math.stat.descriptive.DescriptiveStatistics;
 import org.apache.hadoop.io.Writable;
 
 public class TickerStat implements Writable {
-	Double highestPriceForAnalysisTime;
-	Double lowestPriceForAnalysisTime;
-	Double closePrice;
-	Double medianPrice;
-	Double beta;
-	Double meanPrice;
-	Date beginTime;
-	Date endTime;
-	SimpleDateFormat printFormatter = new SimpleDateFormat(
+	private Double highestPriceForAnalysisTime;
+	private Double lowestPriceForAnalysisTime;
+	private Double closePrice;
+	private Double medianPrice;
+	
+	private Double meanPrice;
+	private Date beginTime;
+	private Date endTime;
+	private SimpleDateFormat printFormatter = new SimpleDateFormat(
 			"dd-MM-yyyy HH-mm-ss");
-	SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
-
 	public TickerStat() {
 
 	}
 
-	public TickerStat(Double highestPriceForAnalysisTime,
-			Double lowestPriceForAnalysisTime, Double closePrice,
+	public TickerStat(ArrayList<Double> highPrices,ArrayList<Double> lowPrices, ArrayList<Double> closePrices,
 			Double medianPrice, Double beta, Double meanPrice, Date beginTime,
 			Date endTime) {
-		this.highestPriceForAnalysisTime = highestPriceForAnalysisTime;
-		this.lowestPriceForAnalysisTime = lowestPriceForAnalysisTime;
-		this.closePrice = closePrice;
-		this.medianPrice = medianPrice;
-		this.beta = beta;
-		this.meanPrice = meanPrice;
+		DescriptiveStatistics stats=new DescriptiveStatistics();
+		ArrayList<Double> prices=new ArrayList<Double>();
+		prices.addAll(highPrices);
+		prices.addAll(lowPrices);
+		prices.addAll(closePrices);
+		for(Double price:prices){
+			stats.addValue(price);
+		}
+		
+		this.highestPriceForAnalysisTime = stats.getMax();
+		this.lowestPriceForAnalysisTime = stats.getMin();
+		this.closePrice = closePrices.get(closePrices.size()-1);
+		this.medianPrice = stats.getPercentile(50);
+
+		this.meanPrice = stats.getMean();
 		this.beginTime = beginTime;
 		this.endTime = endTime;
 
@@ -46,7 +55,7 @@ public class TickerStat implements Writable {
 		lowestPriceForAnalysisTime = in.readDouble();
 		closePrice = in.readDouble();
 		medianPrice = in.readDouble();
-		beta = in.readDouble();
+		
 		meanPrice = in.readDouble();
 		beginTime = new Date(in.readLong());
 		endTime = new Date(in.readLong());
@@ -58,7 +67,7 @@ public class TickerStat implements Writable {
 		out.writeDouble(lowestPriceForAnalysisTime);
 		out.writeDouble(closePrice);
 		out.writeDouble(medianPrice);
-		out.writeDouble(beta);
+	
 		out.writeDouble(meanPrice);
 		out.writeLong(beginTime.getTime());
 		out.writeLong(endTime.getTime());
@@ -72,7 +81,7 @@ public class TickerStat implements Writable {
 				+ " Highest price:" + this.highestPriceForAnalysisTime
 				+ " Lowest price:" + this.lowestPriceForAnalysisTime
 				+ " Close price:" + this.closePrice + " Median price"
-				+ medianPrice + " Mean price" + meanPrice + " Beta" + beta;
+				+ medianPrice + " Mean price" + meanPrice;
 	}
 
 }
